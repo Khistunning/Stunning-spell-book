8183977befcf36805f08ed9768644552074bd836
Khistunning <43241730+Khistunning@users.noreply.github.com>
2019-03-07 09:24:14 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,9 @@
+    $("#office2").click(function() {
+    $(".d-wight").slideToggle();
+});
+    $("#office3").click(function(){
+    $(".office3").toggle();
+});
+    $("#office1").click(function(){
+    $(".office1").fadeToggle();
+});
